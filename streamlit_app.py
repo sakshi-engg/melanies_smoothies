6b9c1a6b96ccc1471fd064bb00d55efd79601ae7
Sakshi Rathi <148839472+sakshi-engg@users.noreply.github.com>
2025-10-12 14:34:12 +0530

    Update streamlit_app.py
    Put the JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
@@ -52,5 +53,6 @@ if ingredients_list:
 # st.write(my_insert_stmt)
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
