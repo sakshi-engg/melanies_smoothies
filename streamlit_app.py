56ace24f499c2637bc0b7fe564b4b682e3f9111f
Sakshi Rathi <148839472+sakshi-engg@users.noreply.github.com>
2025-10-12 15:11:22 +0530

    Update streamlit_app.py
    convert the snowpark dataframe to pandas df so we can use Label based indexing function

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,11 @@ session = cnx.session()
 
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+#convert the snowpark dataframe to pandas df so we can use LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 
