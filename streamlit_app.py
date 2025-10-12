eb504f16b82841543e23804692beb1576833571c
Sakshi Rathi <148839472+sakshi-engg@users.noreply.github.com>
2025-10-12 14:38:59 +0530

    Update streamlit_app.py
    Changing the scope of requests statements block

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 from snowflake.snowpark.context import get_active_session
+import requests
 
 # Write directly to the app
 st.title(f"🥤Customize Your Smoothie!🥤")
@@ -35,7 +36,10 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -51,8 +55,4 @@ if ingredients_list:
 
 
 # st.write(my_insert_stmt)
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
