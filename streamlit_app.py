b72d4942f44deaeb4e6dc0abfef65c2b58aa91ad
Sakshi Rathi <148839472+sakshi-engg@users.noreply.github.com>
2025-10-12 13:48:30 +0530

    Update streamlit_app.py
    Lowercase snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name of your smoothie will be: : ", name_on_order)
 
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 session = get_active_session()
